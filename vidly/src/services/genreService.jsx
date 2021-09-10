5c3b527028a5082c5f2219cbbf4ff8c19345f95a
Omprakash <2019297@iiitdmj.ac.in>
2021-09-10 23:44:06 +0530

    front end under back end

--- vidly/src/services/genreService.jsx
+++
@@ -1,8 +0,0 @@
-import http from "./httpService";
-
-//const endpoint = "http://localhost:3000/api/genres";
-const apiEndpoint = "https://myrental-api.herokuapp.com/api/genres";
-
-export function getGenres() {
-  return http.get(apiEndpoint);
-}
